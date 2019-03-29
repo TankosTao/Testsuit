c3d725a85c20b14aa1227637b875102e4a52fa60
TankosTao <761387278@qq.com>
2019-03-29 17:35:53 +0800

    TankOStao

---
+++ app/src/androidTest/java/com/example/test/MainActivityTest.java
@@ -0,0 +1,43 @@
+package com.example.test;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule= new ActivityTestRule<>(MainActivity.class);
+    private MainActivity mainActivity=null;
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityTestRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+        Log.d("TAG","onCreate()");
+    }
+
+    @Test
+    public void btn_click() {
+        Log.d("TAG","but_click()");
+    }
+    @Test
+    public void testclickbutton() {
+        Espresso.onView(withId(R.id.button)).perform(click());
+  }
+}
